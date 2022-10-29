ddc910b9419cf1b812e3ee863f7321f2f3edb7b3
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-10-29 16:34:53 -0400

    Show the app logo on the splash screen

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/MainActivity.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             EZRecipesTheme {
                 // A surface container using the 'background' color from the theme
