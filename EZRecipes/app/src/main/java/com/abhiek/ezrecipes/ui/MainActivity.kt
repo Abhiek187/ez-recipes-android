42d1a93d7556da5e0deeedea7bf3b79189e84586
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-11-21 23:31:25 -0500

    Create a navigation graph between the home and recipe screens

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/MainActivity.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/MainActivity.kt
@@ -5,22 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.abhiek.ezrecipes.R
-import com.abhiek.ezrecipes.data.MockRecipeService
-import com.abhiek.ezrecipes.data.RecipeRepository
-import com.abhiek.ezrecipes.ui.previews.DevicePreviews
-import com.abhiek.ezrecipes.ui.previews.DisplayPreviews
-import com.abhiek.ezrecipes.ui.previews.FontPreviews
-import com.abhiek.ezrecipes.ui.previews.OrientationPreviews
 import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,56 +34,11 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.padding(padding),
                             color = MaterialTheme.colors.background
                         ) {
-                            Home()
+                            NavigationGraph()
                         }
                     }
                 )
             }
         }
-    }
-}
-
-@Composable
-fun Home(
-    mainViewModel: MainViewModel = viewModel(
-        factory = MainViewModelFactory()
-    )) {
-    val recipe by mainViewModel.recipe.observeAsState()
-    val isLoading by mainViewModel.isLoading.observeAsState(initial = true)
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = { mainViewModel.getRandomRecipe() },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = MaterialTheme.colors.secondary
-            )
-        ) {
-            Text(
-                text = stringResource(R.string.find_recipe_button)
-            )
-        }
-
-        // Show a progress bar while the recipe is loading
-        if (isLoading) {
-            CircularProgressIndicator()
-        }
-    }
-}
-
-@DevicePreviews
-@DisplayPreviews
-@FontPreviews
-@OrientationPreviews
-@Composable
-fun HomePreview() {
-    val viewModel = MainViewModel(RecipeRepository(MockRecipeService))
-
-    EZRecipesTheme {
-        Home(viewModel)
     }
 }
--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/utils/Constants.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/utils/Constants.kt
@@ -4,4 +4,10 @@ object Constants {
     // In Retrofit, base URLs must end with a /
     const val RECIPE_BASE_URL = "https://ez-recipes-server.onrender.com/api/recipes/"
     const val RANDOM_RECIPE_PATH = "random"
+
+    // Routes in the navigation graph
+    object Routes {
+        const val HOME = "home"
+        const val RECIPE = "recipe/{recipe}"
+    }
 }
