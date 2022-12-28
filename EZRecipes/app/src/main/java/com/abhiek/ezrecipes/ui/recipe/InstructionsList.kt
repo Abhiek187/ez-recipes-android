9240ae7bc55ae09bdab80c014efe5c1587e27f9b
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-12-27 22:14:53 -0500

    Start adding the instructions list and step card

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/recipe/InstructionsList.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/recipe/InstructionsList.kt
@@ -1,13 +1,18 @@
 package com.abhiek.ezrecipes.ui.recipe
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.abhiek.ezrecipes.R
 import com.abhiek.ezrecipes.data.MockRecipeService
 import com.abhiek.ezrecipes.data.models.Recipe
 import com.abhiek.ezrecipes.ui.previews.DevicePreviews
@@ -18,13 +23,32 @@ import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 
 @Composable
 fun InstructionsList(recipe: Recipe) {
-    Text(
-        text = recipe.name,
-        fontSize = 24.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding(top = 8.dp)
-    )
+    // Steps heading and cards
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.steps),
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        recipe.instructions.forEach { instruction ->
+            // Split each step by instruction (if applicable)
+            if (instruction.name.isNotEmpty()) {
+                Text(
+                    text = instruction.name,
+                    fontSize = 18.sp
+                )
+            }
+
+            instruction.steps.forEach { step ->
+                StepCard(step)
+            }
+        }
+    }
 }
 
 @DevicePreviews
--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/recipe/StepCard.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/recipe/StepCard.kt
@@ -1,15 +1,21 @@
 package com.abhiek.ezrecipes.ui.recipe
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.abhiek.ezrecipes.R
 import com.abhiek.ezrecipes.data.MockRecipeService
-import com.abhiek.ezrecipes.data.models.Recipe
+import com.abhiek.ezrecipes.data.models.Step
 import com.abhiek.ezrecipes.ui.previews.DevicePreviews
 import com.abhiek.ezrecipes.ui.previews.DisplayPreviews
 import com.abhiek.ezrecipes.ui.previews.FontPreviews
@@ -17,14 +23,100 @@ import com.abhiek.ezrecipes.ui.previews.OrientationPreviews
 import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 
 @Composable
-fun StepCard(recipe: Recipe) {
-    Text(
-        text = recipe.name,
-        fontSize = 24.sp,
-        textAlign = TextAlign.Center,
+fun StepCard(step: Step) {
+    Card(
         modifier = Modifier
-            .padding(top = 8.dp)
-    )
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = 10.dp
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            // Step number and instruction
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    // Center text vertically
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .border(1.dp, MaterialTheme.colors.onBackground, CircleShape)
+                ) {
+                    Text(
+                        text = step.number.toString(),
+                        fontSize = 22.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Text(
+                    text = step.step
+                )
+            }
+
+            if (step.ingredients.isNotEmpty()) {
+                Divider()
+
+                // Ingredients for each step
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.ingredients),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    step.ingredients.forEach { ingredient ->
+                        Column {
+                            AsyncImage(
+                                model = "https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}",
+                                contentDescription = ingredient.name,
+                                modifier = Modifier.size(100.dp)
+                            )
+                            Text(
+                                text = ingredient.name,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+            }
+
+            if (step.equipment.isNotEmpty()) {
+                Divider()
+
+                // Equipment for each step
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.equipment),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    step.equipment.forEach { equipment ->
+                        Column {
+                            AsyncImage(
+                                model = "https://spoonacular.com/cdn/equipment_100x100/${equipment.image}",
+                                contentDescription = equipment.name,
+                                modifier = Modifier.size(100.dp)
+                            )
+                            Text(
+                                text = equipment.name,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 @DevicePreviews
@@ -35,7 +127,7 @@ fun StepCard(recipe: Recipe) {
 fun StepCardPreview() {
     EZRecipesTheme {
         Surface {
-            StepCard(MockRecipeService.recipe)
+            StepCard(MockRecipeService.recipe.instructions[0].steps[0])
         }
     }
 }
