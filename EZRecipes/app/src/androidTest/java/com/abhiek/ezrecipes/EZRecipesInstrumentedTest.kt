bd26b8a1265c270a4e087a81b0f69061684b5108
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-12-31 14:41:12 -0500

    Write an instrumented test for the recipe screen

--- EZRecipes/app/src/androidTest/java/com/abhiek/ezrecipes/EZRecipesInstrumentedTest.kt
+++ EZRecipes/app/src/androidTest/java/com/abhiek/ezrecipes/EZRecipesInstrumentedTest.kt
@@ -1,13 +1,12 @@
 package com.abhiek.ezrecipes
 
 import androidx.activity.compose.setContent
-import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SmallTest
 import com.abhiek.ezrecipes.ui.MainActivity
 import com.abhiek.ezrecipes.ui.MainLayout
 import com.abhiek.ezrecipes.ui.navbar.DrawerItem
@@ -26,6 +25,13 @@ internal class EZRecipesInstrumentedTest {
 
     private lateinit var activity: MainActivity
 
+    private fun printTree() {
+        // Print the Semantics tree
+        composeTestRule
+            .onRoot(useUnmergedTree = true)
+            .printToLog("Tree")
+    }
+
     @Before
     fun init() {
         activity = composeTestRule.activity
@@ -35,6 +41,7 @@ internal class EZRecipesInstrumentedTest {
         }
     }
 
+    @SmallTest
     @Test
     fun homeScreenNavigation() {
         // Check that the hamburger menu and back button on the home screen work as expected
@@ -83,6 +90,135 @@ internal class EZRecipesInstrumentedTest {
         assertTrue(activity.isFinishing)
     }
 
+    @LargeTest
     @Test
-    fun findMeARecipe() {}
+    fun findMeARecipe() {
+        // Click the find recipe button and check that the recipe page renders properly
+        // Large test since it makes a network request and uses quota
+        // When first launching the app, the find recipe button should be clickable
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.find_recipe_button))
+            .assertIsEnabled()
+
+        // After clicking the find recipe button, it should be disabled
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.find_recipe_button))
+            .performClick()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.find_recipe_button))
+            .assertIsNotEnabled()
+
+        // Wait up to 30 seconds for the recipe to load
+        // waitUntil defaults to 1 second before timeout
+        composeTestRule.waitUntil(30_000) {
+            composeTestRule
+                .onAllNodesWithText(activity.getString(R.string.find_recipe_button))
+                .fetchSemanticsNodes()
+                .isEmpty()
+        }
+
+        // Check that the favorite button toggles between filling and un-filling when tapped
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.favorite_alt))
+            .assertExists()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.un_favorite_alt))
+            .assertDoesNotExist()
+
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.favorite_alt))
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.favorite_alt))
+            .assertDoesNotExist()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.un_favorite_alt))
+            .assertExists()
+
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.un_favorite_alt))
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.favorite_alt))
+            .assertExists()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.un_favorite_alt))
+            .assertDoesNotExist()
+
+        // Check that the share button is clickable
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.share_alt))
+            .assertHasClickAction()
+
+        // Since the recipe loaded will be random, check all the elements that are guaranteed
+        // to be there for all recipes
+        // Check that the recipe source link is clickable
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.recipe_link))
+            .assertHasClickAction()
+
+        // Check that the two recipe buttons are clickable
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.made_button))
+            .assertHasClickAction()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.show_recipe_button))
+            .assertHasClickAction()
+
+        // Check that the nutrition label contains all the required nutritional properties
+        for (label in listOf(
+            activity.getString(R.string.nutrition_facts),
+            activity.getString(R.string.calories),
+            activity.getString(R.string.fat),
+            activity.getString(R.string.saturated_fat),
+            activity.getString(R.string.carbohydrates),
+            activity.getString(R.string.fiber),
+            activity.getString(R.string.sugar),
+            activity.getString(R.string.protein),
+            activity.getString(R.string.cholesterol),
+            activity.getString(R.string.sodium)
+        )) {
+            /* More than one match may exist in places like the ingredients list,
+             * but the first match should be in the nutrition label since these matchers
+             * match full strings in EditTexts and TextViews and are case sensitive
+             */
+            composeTestRule
+                .onAllNodesWithText(label)
+                .onFirst()
+                .assertExists()
+        }
+
+        // Check that the summary box, ingredients list, instructions list, and footer are present
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.summary))
+            .assertExists()
+        // "Ingredients" appear in each step card
+        composeTestRule
+            .onAllNodesWithText(activity.getString(R.string.ingredients))
+            .onFirst()
+            .assertExists()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.steps))
+            .assertExists()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.attribution))
+            .assertExists()
+
+        // Check that clicking the show another recipe button disables the button
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.show_recipe_button))
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription(activity.getString(R.string.show_recipe_button))
+            .assertIsNotEnabled()
+        composeTestRule.waitForIdle()
+
+        // Check that clicking the home button in the hamburger menu goes to the home screen
+        composeTestRule
+            .onNodeWithText(DrawerItem.Home.title)
+            .performClick()
+        composeTestRule
+            .onNodeWithText(activity.getString(R.string.find_recipe_button))
+            .assertExists()
+    }
 }
