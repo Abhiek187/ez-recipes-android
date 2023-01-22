c62ffdd4a81951a18e465c79bfd57e32f93170b7
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2023-01-22 18:33:41 -0500

    Pass the width size class from MainActivity

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/MainActivity.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/MainActivity.kt
@@ -3,15 +3,19 @@ package com.abhiek.ezrecipes.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
-            MainLayout()
+            val widthSizeClass = calculateWindowSizeClass(this).widthSizeClass
+            MainLayout(widthSizeClass)
         }
     }
 }
