94d353b4dd9946f248ca0cc028d94798b1750ca2
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-06-23 14:31:57 -0400

    Improve the formatting of the glossary screen

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/glossary/Glossary.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/glossary/Glossary.kt
@@ -1,10 +1,14 @@
 package com.abhiek.ezrecipes.ui.glossary
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.abhiek.ezrecipes.data.models.Term
 import com.abhiek.ezrecipes.ui.previews.DevicePreviews
 import com.abhiek.ezrecipes.ui.previews.DisplayPreviews
@@ -12,12 +16,26 @@ import com.abhiek.ezrecipes.ui.previews.FontPreviews
 import com.abhiek.ezrecipes.ui.previews.OrientationPreviews
 import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 import com.abhiek.ezrecipes.utils.Constants
+import com.abhiek.ezrecipes.utils.boldAnnotatedString
 
 @Composable
 fun Glossary(terms: List<Term>) {
-    LazyColumn {
-        items(terms) { term ->
-            Text(text ="**${term.word}** — ${term.definition}")
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        // Sort all the terms alphabetically for ease of reference
+        itemsIndexed(terms.sortedBy { it.word }) { index, term ->
+            Text(
+                text = boldAnnotatedString(
+                    text = "${term.word} — ${term.definition}",
+                    endIndex = term.word.length
+                ),
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            if (index < terms.lastIndex) {
+                Divider()
+            }
         }
     }
 }
