883d1b2faaef7ae9192e68a0859bddb6431953ef
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-07-06 21:03:40 -0400

    Fix existing MainViewModel tests

--- EZRecipes/app/src/test/java/com/abhiek/ezrecipes/ui/MainViewModelTest.kt
+++ EZRecipes/app/src/test/java/com/abhiek/ezrecipes/ui/MainViewModelTest.kt
@@ -1,7 +1,12 @@
 package com.abhiek.ezrecipes.ui
 
+import android.content.Context
 import com.abhiek.ezrecipes.data.recipe.MockRecipeService
 import com.abhiek.ezrecipes.data.recipe.RecipeRepository
+import com.abhiek.ezrecipes.data.storage.AppDatabase
+import com.abhiek.ezrecipes.data.storage.RecentRecipeDao
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
@@ -11,12 +16,19 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(MainDispatcherExtension::class)
 internal class MainViewModelTest {
     private lateinit var mockService: MockRecipeService
+    private lateinit var recentRecipeDao: RecentRecipeDao
     private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var context: Context
 
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this)
+
         mockService = MockRecipeService
-        viewModel = MainViewModel(RecipeRepository(mockService))
+        recentRecipeDao = AppDatabase.getInstance(context, inMemory = true).recentRecipeDao()
+        viewModel = MainViewModel(RecipeRepository(mockService), recentRecipeDao)
     }
 
     @Test
