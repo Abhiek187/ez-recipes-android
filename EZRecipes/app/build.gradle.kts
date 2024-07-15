b75237b81f35c3a2d4026fcd3595df57548e0244
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-07-14 21:59:34 -0400

    Bump the version name & code

--- EZRecipes/app/build.gradle.kts
+++ EZRecipes/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.abhiek.ezrecipes"
         minSdk = 23
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.0.1"
+        versionCode = 9
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
