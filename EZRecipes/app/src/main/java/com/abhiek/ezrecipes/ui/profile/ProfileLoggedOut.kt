5e5cf8f53bf050e4fff6abff184f3e6f0c274d2d
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-11-02 17:03:17 -0400

    Polish the profile logout screen

--- EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/profile/ProfileLoggedOut.kt
+++ EZRecipes/app/src/main/java/com/abhiek/ezrecipes/ui/profile/ProfileLoggedOut.kt
@@ -1,11 +1,17 @@
 package com.abhiek.ezrecipes.ui.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.abhiek.ezrecipes.R
 import com.abhiek.ezrecipes.ui.previews.DevicePreviews
 import com.abhiek.ezrecipes.ui.previews.DisplayPreviews
@@ -15,13 +21,18 @@ import com.abhiek.ezrecipes.ui.theme.EZRecipesTheme
 
 @Composable
 fun ProfileLoggedOut() {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
         Text(
-            text = stringResource(R.string.login_message)
+            text = stringResource(R.string.login_message),
+            style = MaterialTheme.typography.headlineSmall
         )
 
         Button(
-            onClick = { println("Login") }
+            onClick = { println("Login") },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = stringResource(R.string.login))
         }
