437f5c7bf246345d72bc7112f53b4858d7044014
Abhiek187 <achaudhuri2011@yahoo.com>
2025-02-11 11:21:36 -0500

    Add a toolchain resolver if Java 17 isn't available

--- EZRecipes/settings.gradle.kts
+++ EZRecipes/settings.gradle.kts
@@ -13,6 +13,10 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    // https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
 
 rootProject.name = "EZ Recipes"
 include(":app")
