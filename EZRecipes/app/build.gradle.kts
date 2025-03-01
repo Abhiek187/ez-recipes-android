e77f0d42c3209a55151189a7f345429d4b878418
Abhiek187 <achaudhuri2011@yahoo.com>
2025-03-01 17:41:49 -0500

    Update dependencies

--- EZRecipes/app/build.gradle.kts
+++ EZRecipes/app/build.gradle.kts
@@ -79,14 +79,14 @@ android {
 dependencies {
     val composeBomVersion = "2025.02.00"
     val lifecycleVersion = "2.8.7"
-    val activityVersion = "1.10.0"
+    val activityVersion = "1.10.1"
     val materialVersion = "1.7.8"
     val material3Version = "1.3.1"
     val coroutineVersion = "1.10.1"
     val retrofitVersion = "2.11.0"
     val roomVersion = "2.6.1"
     val googlePlayVersion = "2.0.2"
-    val jupiterVersion = "5.11.4"
+    val jupiterVersion = "5.12.0"
     val espressoVersion = "3.6.1"
 
     implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
@@ -101,8 +101,8 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:$materialVersion")
     implementation("androidx.compose.material3:material3:$material3Version")
     implementation("androidx.compose.material3:material3-window-size-class:$material3Version")
-    implementation("androidx.navigation:navigation-compose:2.8.7")
-    implementation("androidx.datastore:datastore-preferences:1.1.2")
+    implementation("androidx.navigation:navigation-compose:2.8.8")
+    implementation("androidx.datastore:datastore-preferences:1.1.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
 
     // AsyncImage
@@ -127,7 +127,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
-    testImplementation("io.mockk:mockk:1.13.16")
+    testImplementation("io.mockk:mockk:1.13.17")
 
     androidTestImplementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
