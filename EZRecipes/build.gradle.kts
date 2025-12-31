55a375066c8a6dbcfb10228b0355d42b524d1cbb
Abhiek187 <achaudhuri2011@yahoo.com>
2025-12-31 00:29:43 -0500

    Fix unit test & CodeQL builds

--- EZRecipes/build.gradle.kts
+++ EZRecipes/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // Kotlin versions: https://kotlinlang.org/docs/releases.html#release-details
-    val kotlinVersion = "2.3.0"
+    val kotlinVersion = "2.2.21"
 
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
