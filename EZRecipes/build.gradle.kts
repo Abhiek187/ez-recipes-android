6cd08f228cf6b43892ff918e259a2e3a41558b29
Abhiek187 <achaudhuri2011@yahoo.com>
2026-03-01 08:52:31 -0500

    Bump dependencies

--- EZRecipes/build.gradle.kts
+++ EZRecipes/build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     // Kotlin versions: https://kotlinlang.org/docs/releases.html#release-details
-    val kotlinVersion = "2.2.21"
+    val kotlinVersion = "2.3.10"
 
     id("com.android.application") version "9.0.1" apply false
-    id("com.google.devtools.ksp") version "2.3.5" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
     id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
 }
