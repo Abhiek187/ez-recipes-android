639ba7b8d1b26e700d6de04ec468d20d0d21feda
Abhiek187 <achaudhuri2011@yahoo.com>
2026-07-18 19:23:12 -0400

    Add newlines to tests

--- EZRecipes/app/src/test/java/com/abhiek/ezrecipes/data/chef/ProviderTest.kt
+++ EZRecipes/app/src/test/java/com/abhiek/ezrecipes/data/chef/ProviderTest.kt
@@ -1,6 +1,7 @@
 package com.abhiek.ezrecipes.data.chef
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 internal class ProviderTest {
--- EZRecipes/app/src/test/java/com/abhiek/ezrecipes/data/recipe/CuisineTest.kt
+++ EZRecipes/app/src/test/java/com/abhiek/ezrecipes/data/recipe/CuisineTest.kt
@@ -1,6 +1,6 @@
 package com.abhiek.ezrecipes.data.recipe
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class CuisineTest {
