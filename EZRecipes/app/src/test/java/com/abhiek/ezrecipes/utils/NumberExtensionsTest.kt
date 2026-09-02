2e32bf774c63821e75810e64942b1c34da7c416a
Abhiek187 <achaudhuri2011@yahoo.com>
2026-09-01 22:19:29 -0400

    Remove toShorthand tests since the behavior would have to be mocked

--- EZRecipes/app/src/test/java/com/abhiek/ezrecipes/utils/NumberExtensionsTest.kt
+++ EZRecipes/app/src/test/java/com/abhiek/ezrecipes/utils/NumberExtensionsTest.kt
@@ -8,27 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class NumberExtensionsTest {
     @ParameterizedTest
     @CsvSource(
-        "0,0",
-        "1,1",
-        "999,999",
-        "1000,1K",
-        "1234,1.2K",
-        "999999,999.9K",
-        "1000000,1M",
-        "1234567,1.2M",
-        "999999999,999.9M",
-        "1000000000,1B",
-        "1234567890,1.2B"
-    )
-    fun toShorthand(inputNum: Int, expectedStr: String) {
-        // Android methods can't be mocked, so this just tests the else block
-        // Build.VERSION.SDK_INT will always be 0 in unit tests
-        val actualStr = inputNum.toShorthand()
-        assertEquals(expectedStr, actualStr)
-    }
-
-    @ParameterizedTest
-    @CsvSource(
         "0,0,0",
         "0.5,0,1",
         "1.8175,2,1.82",
@@ -64,6 +43,8 @@ internal class NumberExtensionsTest {
         // When converted to an ISO string
         val actualISOString = timestamp.toISODateString()
         // Then it should match the expected value
+        // Note: Android methods can't be mocked, so this just tests the else block
+        // Build.VERSION.SDK_INT will always be 0 in unit tests
         assertEquals(expectedISOString, actualISOString)
     }
 }
